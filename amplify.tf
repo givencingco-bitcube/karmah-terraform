d479395653f29914ede56265c0187b408f826244
givencingco-bitcube <given@bitcube.tech>
2024-11-13 15:22:57 +0200

    dist not working as base directory

--- amplify.tf
+++ amplify.tf
@@ -7,24 +7,22 @@ resource "aws_amplify_app" "karmah_amplify" {
 
   # The default build_spec added by the Amplify Console for React.
   build_spec = <<-EOT
- version: 1
-frontend:
-  phases:
-    preBuild:
-      commands:
-        - npm ci --cache .npm --prefer-offline
-    build:
-      commands:
-        - npm run build
-  artifacts:
-    baseDirectory: build
-    files:
-      - '**/*'
-  cache:
-    paths:
-      - .next/cache/**/*
-      - .npm/**/*
-
+    version: 0.1
+    frontend:
+      phases:
+        preBuild:
+          commands:
+            - yarn install
+        build:
+          commands:
+            - yarn run build
+      artifacts:
+        baseDirectory: build
+        files:
+          - '**/*'
+      cache:
+        paths:
+          - node_modules/**/*
   EOT
 
   # The default rewrites and redirects added by the Amplify Console.
